ef9abd707b5d924f6518bbe903a528d5e781d845
yoshihiko sughimura <yoikosugi@gmail.com>
2019-05-14 18:56:34 +0900

    Add page of About

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   get "top" => "home#top"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "about" => "home#about"
 end
