e95da0222de245868c043a68f2dda2a298ef3c1a
yoshihiko sughimura <yoikosugi@gmail.com>
2019-05-15 20:52:35 +0900

    add create to posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @posts = Post.all.order(created_at: :desc)
   end
 
   def show
@@ -9,5 +9,11 @@ class PostsController < ApplicationController
 
   def new
 
+  end
+
+  def create
+    @post = Post.new(content: params[:content])
+    @post.save
+    redirect_to("/posts/index")
   end
 end
