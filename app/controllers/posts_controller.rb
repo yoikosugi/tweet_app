d0668f5ebb77b628d241590da85681b00d23aa94
yoshihiko sughimura <yoikosugi@gmail.com>
2019-05-15 23:30:11 +0900

    add update to posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,4 +20,11 @@ class PostsController < ApplicationController
   def edit
     @post = Post.find_by(id: params[:id])
   end
+
+  def update
+    @post = Post.find_by(id: params[:id])
+    @post.content = params[:content]
+    @post.save
+    redirect_to("/posts/index")
+  end
 end
