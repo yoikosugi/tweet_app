cd45004e443469d7ed387e4968aa888ceb51a7dd
yoshihiko sughimura <yoikosugi@gmail.com>
2019-05-16 03:14:30 +0900

    add destroy to posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -27,4 +27,10 @@ class PostsController < ApplicationController
     @post.save
     redirect_to("/posts/index")
   end
+
+  def destroy
+    @post = Post.find_by(id: params[:id])
+    @post.destroy
+    redirect_to("/posts/index")
+  end
 end
