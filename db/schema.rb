59c268bb78ce3e2fdaffef2e49e1ff0d1b361e3a
yoshihiko sughimura <yoikosugi@gmail.com>
2019-05-16 22:47:06 +0900

    add image name to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_16_072335) do
+ActiveRecord::Schema.define(version: 2019_05_16_134402) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2019_05_16_072335) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_name"
   end
 
 end
