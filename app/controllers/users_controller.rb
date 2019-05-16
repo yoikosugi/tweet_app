fdb7e30c81e4e11c2613226826e15fa8307e88f2
yoshihiko sughimura <yoikosugi@gmail.com>
2019-05-17 03:33:54 +0900

    add login&logout to users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,15 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user, {only: [:index, :show, :edit, :update]}
+  before_action :forbid_login_user, {only: [:new, :create, :login_form, :login]}
+  before_action :ensure_correct_user, {only: [:edit, :update]}
+
+  def ensure_correct_user
+    if @current_user.id != params[:id].to_i
+      flash[:notice] = "権限がありません"
+      redirect_to("/posts/index")
+    end
+  end
+
   def index
     @users = User.all
   end
@@ -15,9 +26,11 @@ class UsersController < ApplicationController
     @user = User.new(
       name: params[:name], 
       email: params[:email], 
-      image_name: "default_user.jpg"
+      image_name: "default_user.jpg",
+      password: params[:password]
       )
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "ユーザー登録が完了しました"
       redirect_to("/users/#{@user.id}")
     else
@@ -62,5 +75,11 @@ class UsersController < ApplicationController
       @password = params[:password]
       render("users/login_form")
     end
+  end
+
+  def logout
+    session[:user_id] = nil
+    flash[:notice] = "ログアウトしました"
+    redirect_to("/login")
   end
 end
