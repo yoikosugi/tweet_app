bf23074eddd0100df048494083265a9d8e84e09a
yoshihiko sughimura <yoikosugi@gmail.com>
2019-05-16 22:37:38 +0900

    add edit to users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  post "users/:id/update" => "users#update"
+  get "users/:id/edit" => "users#edit"
   get "signup" => "users#new"
   post "users/create" => "users#create"
   get "users/index" => "users#index"
