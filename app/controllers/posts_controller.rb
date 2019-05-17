79ea3f3cb34c43c4970a3aa72f7016df71f210d8
yoshihiko sughimura <yoikosugi@gmail.com>
2019-05-17 22:01:30 +0900

    Make relation between users & posts, Add user to posts.rb & Add posts to user.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,14 @@
 class PostsController < ApplicationController
   before_action :authenticate_user
+  before_action :ensure_correct_user, {only: [:edit, :update, :destroy]}
+
+  def ensure_correct_user
+    @post = Post.find_by(id: params[:id])
+    if @post.user_id != @current_user.id
+      flash[:notice] = "権限がありません"
+      redirect_to("/posts/index")
+    end
+  end
 
   def index
     @posts = Post.all.order(created_at: :desc)
@@ -7,6 +16,7 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find_by(id: params[:id])
+    @user = @post.user
   end
 
   def new
@@ -14,7 +24,7 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new(content: params[:content])
+    @post = Post.new(content: params[:content], user_id: @current_user.id)
     if @post.save
       flash[:notice] = "投稿を作成しました"
       redirect_to("/posts/index")
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
   validates :name, {presence: true}
   validates :email, {presence: true, uniqueness: true}
+
+  def posts
+    return Post.where(user_id: self.id)
+  end
 end
