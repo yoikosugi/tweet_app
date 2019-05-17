0427a73b418273474f6b61da772e538eb2018aa6
yoshihiko sughimura <yoikosugi@gmail.com>
2019-05-18 05:41:58 +0900

    Add authentication of password

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_17_120540) do
+ActiveRecord::Schema.define(version: 2019_05_17_201759) do
 
   create_table "likes", force: :cascade do |t|
     t.integer "user_id"
@@ -32,7 +32,7 @@ ActiveRecord::Schema.define(version: 2019_05_17_120540) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image_name"
-    t.string "password"
+    t.string "password_digest"
   end
 
 end
